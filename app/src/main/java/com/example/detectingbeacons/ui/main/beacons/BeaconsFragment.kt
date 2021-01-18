a4adc284f18d9079bc58b3b50ef775d71bac1b3b
borja <borja_orts_bosch@hotmail.com>
2021-01-18 22:57:54 +0100

    new layout

--- app/src/main/java/com/example/detectingbeacons/ui/main/beacons/BeaconsFragment.kt
+++ app/src/main/java/com/example/detectingbeacons/ui/main/beacons/BeaconsFragment.kt
@@ -19,12 +19,21 @@ import androidx.fragment.app.Fragment
 import com.example.detectingbeacons.R
 import com.example.detectingbeacons.databinding.FragmentBeaconsBinding
 import org.altbeacon.beacon.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 
 private const val ALL_BEACONS_REGION = "AllBeaconsRegion"
 private const val REQUEST_ENABLE_BT = 1
 private const val DEFAULT_SCAN_PERIOD_MS = 6000L
 private const val TAG = "BeaconFragment"
+const val RUUVI_LAYOUT = "m:0-2=0499,i:4-19,i:20-21,i:22-23,p:24-24" // TBD
+const val IBEACON_LAYOUT = "m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"
+const val ALTBEACON_LAYOUT = BeaconParser.ALTBEACON_LAYOUT
+const val ALBEACON_LAYOUT2 = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+const val EDDYSTONE_UID_LAYOUT = BeaconParser.EDDYSTONE_UID_LAYOUT
+const val EDDYSTONE_URL_LAYOUT = BeaconParser.EDDYSTONE_URL_LAYOUT
+const val EDDYSTONE_TLM_LAYOUT = BeaconParser.EDDYSTONE_TLM_LAYOUT
 
 class BeaconsFragment : Fragment(), BeaconConsumer, RangeNotifier {
 
@@ -49,10 +58,14 @@ class BeaconsFragment : Fragment(), BeaconConsumer, RangeNotifier {
 
         //Se inicializa el objeto BeaconManager con la configuracion AltBeacon y la Region
         mBeaconManager = BeaconManager.getInstanceForApplication(requireContext())
-        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT))
-        //"m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
-        //m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"
-        //"m:0-3=2d24bf16"
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(IBEACON_LAYOUT))
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(EDDYSTONE_UID_LAYOUT))
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(EDDYSTONE_URL_LAYOUT))
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(EDDYSTONE_TLM_LAYOUT))
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(ALBEACON_LAYOUT2))
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(RUUVI_LAYOUT))
+        mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(ALTBEACON_LAYOUT))
+
         val identifiers = ArrayList<Identifier>()
         mRegion = Region(ALL_BEACONS_REGION, identifiers)
 
@@ -176,13 +189,39 @@ class BeaconsFragment : Fragment(), BeaconConsumer, RangeNotifier {
             val nearestBeacon = beacons.minByOrNull {
                 it.distance
             }
+            val distanceRound = nearestBeacon?.distance
+
+            val uuid = try {
+                nearestBeacon?.id1?.toString()
+            } catch (e: IndexOutOfBoundsException) {
+                "0"
+            }
+            val major = try {
+                nearestBeacon?.id2?.toString()
+            } catch (e: IndexOutOfBoundsException) {
+                "0"
+            }
+            val minor = try {
+                nearestBeacon?.id3?.toString()
+            } catch (e: IndexOutOfBoundsException) {
+                "0"
+            }
+
+            //Asignar datos al CardView
             binding.cardView.visibility = View.VISIBLE
-            binding.distanceText.text = nearestBeacon?.distance.toString()
-            binding.uuidText.text = nearestBeacon?.id1.toString()
-            binding.majorText.text = nearestBeacon?.bluetoothAddress.toString()
-            binding.minorTxt.text = nearestBeacon?.rssi.toString()
-           // showToastMessage("Distancia: ${nearestBeacon?.distance.toString()}")
-           // binding.textViewBeacon.text = "${nearestBeacon?.distance.toString()} ${nearestBeacon?.bluetoothAddress.toString()}"
+            binding.distanceText.text = if (distanceRound != null) {
+                "${BigDecimal(distanceRound).setScale(2, RoundingMode.HALF_EVEN)} m"
+            } else {
+                "Error al leer el beacon"
+            }
+
+            binding.bluetoothAddress.text = nearestBeacon?.bluetoothAddress.toString()
+            binding.uuidText.text = "UUID: $uuid"
+            binding.majorText.text = "Mayor: $major"
+            binding.minorTxt.text = "Minor: $minor"
+            binding.rssi.text = "RSSI: ${nearestBeacon?.rssi} dBm"
+            binding.tx.text = "Tx: ${nearestBeacon?.txPower} dBm"
+
         }
     }
 
